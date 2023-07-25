e02c7dc8c6d551d94fda32f29a581650da6aee41
Joel Boschetti <boschettijoel@gmail.com>
2023-07-25 19:48:33 -0300

    asd

--- main.js
+++ main.js
@@ -1,47 +1,18 @@
 // bienvenida
 alert('bienvenido a 9zShop')
-const comprarProductos = () => {
-    let producto = ''
-    let cantidad = 0
-    let precio = 0
-    let subtotal = 0
-    let seguirComprando = true
+const carritoCompras = [];
+  const ordenarMayorMenor = () =>{
+    productos.sort((a,b)=>b.precio - a.precio);
+    mostrarListaOrdenada();
+  }
 
 
-    do {
-        producto = prompt('Queres comprar Camiseta, Gorra o Ambos?').toUpperCase()
-        cantidad = parseInt(prompt('¿Cuantos quieres comprar?'))
-        console.log(producto);
-        console.log(cantidad);
-        while (Number.isNaN(cantidad) || cantidad === 0){
-            alert('Debe agregar una cantidad válida!')
-            cantidad = parseInt(prompt('¿Cuantos queres comprar?'))
-           }
-        
-        switch (producto) {
-            case 'CAMISETA':
-                precio = 8600
-                break;
-            case 'GORRA':
-                precio = 3000
-                break;
-                case 'AMBOS':
-                precio = 30000
-                break;
-            default:
-                alert('Alguno de los datos ingresados son incorrectos')
-                precio = 0
-                cantidad = 0
-                
-        }
-        subtotal += precio * cantidad
-
-        seguirComprando = confirm('¿Deseas seguir comprando?')
-    } while (seguirComprando);
-
-    return subtotal
-}
-
-let resultado = comprarProductos()
-alert("Este es tu total: $" +resultado)
-console.log(resultado);
+    const mostrarListaOrdenada = () =>{
+        const listaOrdenada = productos.map(producto =>{
+return '- '+ producto.nombre+ ' $' +producto.precio
+        });
+        console.log(listaOrdenada);
+        // alert('Lista de precios'+'\n\n'+listaOrdenada.join('\n'))
+        comprarProductos(listaOrdenada)
+    }
+    ordenarMayorMenor
---
+++ stock.js
@@ -0,0 +1,29 @@
+const productos = [
+
+
+    {
+id: 1,
+nombre: "Camiseta 9z 2021",
+tipo: "camiseta",
+precio: 1200,
+talle: "XL",
+cantidad: 0
+
+
+    },
+{
+    id: 2,
+nombre: "Gorra 9z",
+tipo: "Gorra",
+precio: 2200,
+talle: "ajustable",
+cantidad: 0
+},
+{id: 3,
+    nombre: "Campera 9z",
+    tipo: "campera",
+    precio: 5200,
+    talle: "XL",
+    cantidad: 0}
+    
+]
