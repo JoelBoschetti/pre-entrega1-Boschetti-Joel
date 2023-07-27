efaa9fdd052e84c8bb8d81738f6bcb9c1cc46ef6
Joel Boschetti <boschettijoel@gmail.com>
2023-07-27 20:08:04 -0300

    final 2nda entrega

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
 // bienvenida
-alert('bienvenido a 9zShop')
-const carritoCompras = [];
-  const ordenarMayorMenor = () =>{
-    productos.sort((a,b)=>b.precio - a.precio);
+
+const carrito = [];
+
+
+
+  const ordenarMayorMenor = () => {
+    productos.sort((a,b)=> a.precio - b.precio);
     mostrarListaOrdenada();
   }
 
+  const ordenarMenorMayor = () => {
+    productos.sort((a,b)=> b.precio - a.precio);
+    mostrarListaOrdenada();
+  }
 
-    const mostrarListaOrdenada = () =>{
-        const listaOrdenada = productos.map(producto =>{
-return '- '+ producto.nombre+ ' $' +producto.precio
-        });
-        console.log(listaOrdenada);
-        // alert('Lista de precios'+'\n\n'+listaOrdenada.join('\n'))
-        comprarProductos(listaOrdenada)
+    const mostrarListaOrdenada = () => {
+   
+         const listaOrdenada = productos.map(producto => {
+  return '- '+producto.nombre+' $' +producto.precio
+           });
+           
+            alert('Lista de precios'+'\n\n'+listaOrdenada.join('\n'))
+            comprarProductos(listaOrdenada)
     }
-    ordenarMayorMenor
+    const comprarProductos = (listaOrdenada) => {
+      let otroProducto = false
+      let productoNombre = ''
+      let productoCantidad = ''
+    
+    do {
+      productoNombre= prompt('¿que producto quiere comprar?'+'\n\n'+listaOrdenada.join('\n'))
+      productoCantidad = parseInt(prompt('cuantos vas a comprar?'))
+     const existeProducto = productos.some(producto => producto.nombre.toLowerCase() == productoNombre.toLowerCase())
+     if (existeProducto) {
+      const producto = productos.find(producto => producto.nombre.toLowerCase() == productoNombre.toLowerCase())
+      agregarAlCarrito(producto, productoCantidad)
+     } else {
+      alert("El producto no esta en el catalogo")
+     }
+     otroProducto=confirm (alert("Quiere seguir comprando?"))
+    } while (otroProducto);
+}
+const agregarAlCarrito = (producto, productoCantidad) => {
+  const productoId = producto.id 
+  const productoRepetido= carrito.find(producto => producto.id == productoId)
+  if (!productoRepetido) {
+    producto.cantidad += productoCantidad
+    carrito.push(producto)
+  } else {
+    productoRepetido.cantidad += productoCantidad
+  }
+  console.log(carrito)
+}
+    ordenarMayorMenor()
--- stock.js
+++ stock.js
@@ -5,16 +5,19 @@ const productos = [
 id: 1,
 nombre: "Camiseta 9z 2021",
 tipo: "camiseta",
+descripcion: "una camiseta hermosa",
 precio: 1200,
 talle: "XL",
 cantidad: 0
 
 
     },
+    
 {
     id: 2,
 nombre: "Gorra 9z",
 tipo: "Gorra",
+descripcion: "una gorra hermosa",
 precio: 2200,
 talle: "ajustable",
 cantidad: 0
@@ -22,8 +25,11 @@ cantidad: 0
 {id: 3,
     nombre: "Campera 9z",
     tipo: "campera",
-    precio: 5200,
+    descripcion: "una campera hermosa",
+    precio: 200,
     talle: "XL",
-    cantidad: 0}
+    cantidad: 0
+}
     
 ]
+console.log(productos)
