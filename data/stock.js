3118185cb041445a1e5e2ee29c4283ca54be3837
Joel Boschetti <boschettijoel@gmail.com>
2023-08-08 19:19:35 -0300

    trabajo hecho

---
+++ data/stock.js
@@ -0,0 +1,36 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Remera 9z",
+        tipo: "Remera 9z",
+        desc: "Una campera re linda",
+        precio: 1200,
+        talle: "L",
+        imagen: '../public/images/descarga.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Camiseta 9z ",
+        tipo: "camiseta 9z",
+        desc: "Una re camiseta papa",
+        precio: 500,
+        talle: "L",
+        imagen: '../public/images/camiseta.png',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Buzo 9z",
+        tipo: "buzo de 9z",
+        desc: "Una camisa que re va con vos",
+        precio: 900,
+        talle: "S",
+        imagen: '../public/images/buzo9z.png',
+        cantidad:1
+    },
+
+];
